391b0fd77ce9d5bb9a674f6619cbf708d78d8d21
Danny Nguyen <danny@80pct.com>
2022-09-10 08:45:54 -0400

    Setup Base Web

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
+
+const engine = new Styletron();
+
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <Centered>
+        <App />
+      </Centered>
+    </BaseProvider>
+  </StyletronProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
